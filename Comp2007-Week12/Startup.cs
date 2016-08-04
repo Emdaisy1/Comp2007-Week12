cf9a8e574d7cb6716fdb9fce51ac0de75e5958f4
Emdaisy1 <hilborn_emmav@live.ca>
2016-08-04 09:42:41 -0400

    Add project files.

---
+++ Comp2007-Week12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2007_Week12.Startup))]
+namespace Comp2007_Week12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
